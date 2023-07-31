bf9b0c537162d40bb8320bf03bb7a3508a97efcd
KLohger <fanimdude32@gmail.com>
2023-07-31 16:44:42 +0200

    Updated main module
    Fixed TypeError in MakeBayer introduced in Python3 where floats can't be used for indexing. Fixed by switching to integer divison.
    
    Split off png outputting and terminal printing functionality from MakeBayer into separate functions PrintMatrix and OutputToPng.
    
    Removed function MakeBayer in favor of simply using InitBayer with default arguments.
    
    Changed name of function InitBayer to MakeBayer, since original wrapper function MakeBayer no longer exists.
    
    Updated original matrix printing code to be printed out in rows and columns. it's currently a bit hardcoded, but the printing was most likely made for debug purposes anyway.
    
    Updated PNG outputting code to support defining path's and to optionally not normalize brightness.
    Also removed hardcoded brightness multiplication map.
    Also changed image mode to "L".
    
    Added typing for entire module.

--- MakeBayer.py
+++ MakeBayer.py
@@ -1,56 +1,76 @@
-def InitBayer(x, y, size, value, step,
-              matrix = [[]]):
-    if matrix == [[]]:
-        matrix = [[0 for i in range(size)]for i in range(size)]
-    
-    if (size == 1):
+from typing import Final
+
+
+Matrix = list[list[int]]
+
+
+def MakeBayer(
+    size: int = 16,
+    x: int = 0,
+    y: int = 0,
+    value: int = 0,
+    step: int = 1,
+    matrix: Matrix | None = None,
+):
+    if matrix is None:
+        matrix = [[0 for _ in range(size)] for _ in range(size)]
+
+    if size == 1:
         matrix[y][x] = value
-        return
-    
-    half = size/2
-    
-    #subdivide into quad tree and call recursively
-    #pattern is TL, BR, TR, BL
-    InitBayer(x,      y,      half, value+(step*0), step*4, matrix)
-    InitBayer(x+half, y+half, half, value+(step*1), step*4, matrix)
-    InitBayer(x+half, y,      half, value+(step*2), step*4, matrix)
-    InitBayer(x,      y+half, half, value+(step*3), step*4, matrix)
+        return matrix
+
+    half = size // 2
+
+    # subdivide into quad tree and call recursively
+    MakeBayer(half,     x,          y, value + (step * 0),  step * 4,           matrix)             # Top Left
+    MakeBayer(half,     x + half,   y + half,               value + (step * 1), step * 4, matrix)   # Bottom Right
+    MakeBayer(half,     x + half,   y, value + (step * 2),  step * 4,           matrix)             # Top Right
+    MakeBayer(half,     x,          y + half,               value + (step * 3), step * 4, matrix)   # Bottom Left
     return matrix
 
-def MakeBayer(matrixSize, savePng, pngTileCount):
-    pngFilename = 'bayer%d.png' % matrixSize
-    if (pngTileCount > 1):
-        pngFilename = 'bayer%dtile%d.png' % (matrixSize, pngTileCount)
 
-    matrix = InitBayer(0, 0, matrixSize, 0, 1)
-    
-    if (savePng):
-        from PIL import Image
-        brightnessMultiplier = {16:1,8:4,4:16,2:64}
-        img = Image.new('RGB', (matrixSize*pngTileCount,
-                                matrixSize*pngTileCount))
-        imgData = img.load()
-        for y in range(img.height):
-            for x in range(img.width):
-                value = matrix[y % matrixSize][x % matrixSize]
-                value *= brightnessMultiplier[matrixSize]
-                color = (value, value, value)
-                imgData[x,y] = color
-        img.save(pngFilename, "PNG")
-        print('Saved %s' % pngFilename)
-    else:
-        print('Bayer Matrix %s' % matrixSize)
-        print(matrix)
+def PrintMatrix(matrix: Matrix):
+    print(f"Bayer Matrix {matrix.__len__()}")
+    for row in matrix:
+        print(*list(map(lambda n: str(n).ljust(4), row)), sep="")
 
 
-if __name__ == '__main__':
+def OutputToPng(
+    matrix: Matrix,
+    tileCount: int = 1,
+    path: str = "",
+    normalizeBrightness: bool = True,
+):
+    from PIL import Image
+
+    matrixSize = matrix.__len__()
+    pngFilename = (
+        
+        f"{path}/bayer{matrixSize}{f'tile{tileCount}' if tileCount > 1 else '' }.png"
+    )
+
+    img = Image.new("L", (matrixSize * tileCount, matrixSize * tileCount))
+    imgData = img.load()
+    for y in range(img.height):
+        for x in range(img.width):
+            color = matrix[y % matrixSize][x % matrixSize]
+            if normalizeBrightness:
+                color *= 256 / (matrixSize * matrixSize)
+
+            imgData[x, y] = color
+    img.save(pngFilename, "PNG")
+    print("Saved %s" % pngFilename)
+
+
+if __name__ == "__main__":
     # size should be a power of two.
     # note that matrix sizes larger than 16 go out of 0..255 range,
     # so image export will not work correctly
-    sizes = 2,4,8,16
-    
-    tileCounts = 1,2,4,8,16
+    sizes = 2, 4, 8, 16
+
+    tileCounts = 1, 2, 4, 8, 16
 
     for size in sizes:
         for tileCount in tileCounts:
-            MakeBayer(matrixSize=size, savePng=True, pngTileCount=tileCount)
+            matrix = MakeBayer(size)
+            OutputToPng(matrix, tileCount, path="images")
