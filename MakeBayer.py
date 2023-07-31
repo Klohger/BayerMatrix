5ca653e41d6b059452a63d77ec8649d7072282ec
KLohger <fanimdude32@gmail.com>
2023-07-31 21:46:50 +0200

    Generalized OutputToPNG
    Generalized and renamed function OutputToPng to SaveAsImage. It now supports multiple different colour modes and image formats and as such can output matrices where values exceed 255.

--- MakeBayer.py
+++ MakeBayer.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+from typing import Literal
+
+from PIL import Image
+
 Matrix = list[list[int]]
 
 
@@ -17,12 +22,13 @@ def MakeBayer(
         return matrix
 
     half = size // 2
-
-    # subdivide into quad tree and call recursively
-    MakeBayer(half,     x,          y, value + (step * 0),  step * 4,           matrix)             # Top Left
-    MakeBayer(half,     x + half,   y + half,               value + (step * 1), step * 4, matrix)   # Bottom Right
-    MakeBayer(half,     x + half,   y, value + (step * 2),  step * 4,           matrix)             # Top Right
-    MakeBayer(half,     x,          y + half,               value + (step * 3), step * 4, matrix)   # Bottom Left
+    # fmt: off
+    # subdivide into quad tree and call recursively 
+    MakeBayer(size=half,     x=x,          y=y,         value=value + (step * 0),   step=step * 4, matrix=matrix)   # Top Left
+    MakeBayer(size=half,     x=x + half,   y=y + half,  value=value + (step * 1),   step=step * 4, matrix=matrix)   # Bottom Right
+    MakeBayer(size=half,     x=x + half,   y=y,         value=value + (step * 2),   step=step * 4, matrix=matrix)   # Top Right
+    MakeBayer(size=half,     x=x,          y=y + half,  value=value + (step * 3),   step=step * 4, matrix=matrix)   # Bottom Left
+    # fmt: on
     return matrix
 
 
@@ -32,42 +38,68 @@ def PrintMatrix(matrix: Matrix):
         print(*list(map(lambda n: str(n).ljust(4), row)), sep="")
 
 
-def OutputToPng(
+def SaveAsImage(
     matrix: Matrix,
     tileCount: int = 1,
-    path: str = "",
-    normalizeBrightness: bool = True,
+    mode: Literal["L"] | Literal["I"] | Literal["F"] = "L",
+    folder: Path = Path.cwd(),
+    ext: str = ".png",
+    format: str = "PNG",
+    normalize: bool = True,
 ):
-    from PIL import Image
-
     matrixSize = matrix.__len__()
-    pngFilename = (
-        
-        f"{path}/bayer{matrixSize}{f'tile{tileCount}' if tileCount > 1 else '' }.png"
+    outputPath = (
+        folder / f"bayer{matrixSize}{f'tile{tileCount}' if tileCount > 1 else '' }{ext}"
     )
 
-    img = Image.new("L", (matrixSize * tileCount, matrixSize * tileCount))
+    img = Image.new(mode, size=(matrixSize * tileCount, matrixSize * tileCount), color=None)  # type: ignore
     imgData = img.load()
     for y in range(img.height):
         for x in range(img.width):
             color = matrix[y % matrixSize][x % matrixSize]
-            if normalizeBrightness:
-                color *= 256 / (matrixSize * matrixSize)
+            if normalize:
+                match mode:
+                    case "L":
+                        color = (color * (256 / (matrixSize * matrixSize))).__floor__()
+                    case "I":
+                        # so apparently the docs are lying and in i mode 'I' each pixel is represented by a 16 bit unsigned integer
+                        color = (
+                            color * (65536 / (matrixSize * matrixSize))
+                        ).__floor__()
+                    case "F":
+                        color *= 1 / (matrixSize * matrixSize)
 
             imgData[x, y] = color
-    img.save(pngFilename, "PNG")
-    print("Saved %s" % pngFilename)
+    img.save(outputPath, format)
+    print(f"Saved {outputPath}")
 
 
 if __name__ == "__main__":
     # size should be a power of two.
     # note that matrix sizes larger than 16 go out of 0..255 range,
     # so image export will not work correctly
-    sizes = 2, 4, 8, 16
+    sizesLowerThan32 = 2, 4, 8, 16
 
     tileCounts = 1, 2, 4, 8, 16
 
-    for size in sizes:
+    for size in sizesLowerThan32:
         for tileCount in tileCounts:
             matrix = MakeBayer(size)
-            OutputToPng(matrix, tileCount, path="images")
+            SaveAsImage(
+                matrix,
+                tileCount,
+                folder=Path("./images"),
+                mode="L",
+            )
+
+    sizesHigherThan16 = 32, 64, 128, 256
+
+    for size in sizesHigherThan16:
+        for tileCount in tileCounts:
+            matrix = MakeBayer(size)
+            SaveAsImage(
+                matrix,
+                tileCount,
+                folder=Path("./images"),
+                mode="I",
+            )
